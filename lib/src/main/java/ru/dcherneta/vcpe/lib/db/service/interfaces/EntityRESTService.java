8c2cf84f949561b18627576ce599f9ae5ccd6513
DCherneta <DChereneta@beeline.ru>
2017-02-15 10:32:23 +0300

    не получается отловить причину ошибки:
    [2017-02-14T18:53:06.693+0300] [WARNING] [] [javax.enterprise.web] [tid: _ThreadID=28 _ThreadName=http-thread-pool(4)] [timeMillis: 1487087586693] [levelValue: 900] [[
      StandardWrapperValve[ru.dcherneta.vcpe.lib.db.service.RestApplication]: Servlet.service() for servlet ru.dcherneta.vcpe.lib.db.service.RestApplication threw exception
    java.lang.IllegalArgumentException: object is not an instance of declaring class
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)

---
+++ lib/src/main/java/ru/dcherneta/vcpe/lib/db/service/interfaces/EntityRESTService.java
@@ -0,0 +1,29 @@
+package ru.dcherneta.vcpe.lib.db.service.interfaces;
+
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by DCherneta on 14.02.2017.
+ */
+public interface EntityRESTService<T> {
+
+    public default T create(T entity){
+        throw new UnsupportedOperationException("Method delete is NOT supporting");
+    };
+
+    public default T delete(Long id){
+        throw new UnsupportedOperationException("Method DELETE is NOT supporting");
+    };
+
+    public default T get(int id){
+        throw new UnsupportedOperationException("Method GET is NOT supporting");
+    } ;
+/*
+    public default Response get(int id){
+        throw new UnsupportedOperationException("Method GET is NOT supporting");
+    } ;*/
+
+    public default T update(T entity){
+        throw new UnsupportedOperationException("Method UPDATE is NOT supporting");
+    };
+}
